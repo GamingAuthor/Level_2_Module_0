287d0c8a29e21cb7672d920e7af922fdfac9bb1c
Centehua <league@iMac.attlocal.net>
2018-02-18 12:51:28 -0800

    Booking Guests!
    Cooking Geese?

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,15 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +18,41 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add Name");
+	JButton view = new JButton("View Names");
+	String names;
+	ArrayList<String> nam = new ArrayList<String>();
+	public static void main(String[] args) {
+		GuestBook demetri = new GuestBook();
+		demetri.ui();
+	}
+	void ui() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		add.addActionListener(this);
+		view.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(add)) {
+			names = JOptionPane.showInputDialog("Enter in the name of a guest.");
+			nam.add(names);
+		}
+		if(e.getSource().equals(view)) {
+		String ok = "";
+			for(int i = 0; i<nam.size();i++) {
+				int x = i;
+				String lol = "Guest #"+(x+=1)+": "+nam.get(i)+"\n";	
+				ok+=lol;
+			}
+			JOptionPane.showMessageDialog(null, ok);
+		}
+	}
 }
